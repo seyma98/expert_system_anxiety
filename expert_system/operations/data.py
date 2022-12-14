b8a233a2d580a15072d3322e5fb30e073a0093f9
seyma98 <79853810+seyma98@users.noreply.github.com>
2022-12-15 01:44:31 +0300

    Add files via upload

--- expert_system/operations/data.py
+++ expert_system/operations/data.py
@@ -14,3 +14,32 @@ def takeQuestions():
         rule_no+=1
 
     return theList
+
+# function to add to JSON
+def write_json(new_data, filename="static/jsonFiles/knowledge.json"):  
+     with open(filename,'r+') as file:
+        # First we load existing data into a dict.
+        file_data = json.load(file)
+        # Join new_data with file_data inside emp_details
+        file_data["target"].append(new_data)
+
+        file.seek(0)
+        # convert back to json.
+        json.dump(file_data, file, indent = 7)
+
+def write_question_json(filename="static/jsonFiles/questions.json"):  
+     with open(filename,'r+') as file:
+        # First we load existing data into a dict.
+        file_data = json.load(file)
+        # Join new_data with file_data inside emp_details
+
+        file_data["target"][0]["rules"]["46"] = "46"
+
+
+        file.seek(0)
+        # convert back to json.
+        json.dump(file_data, file, indent = 4)
+
+
+
+
